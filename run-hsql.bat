a39a6db833e4baada78b3d42e2201b85fbfff9f9
Guido Zockoll <Guido Zockoll@payone.de>
2011-11-24 16:49:33 +0100

    Update to actual jars

---
+++ run-hsql.bat
@@ -0,0 +1,1 @@
+java -cp "%USERPROFILE%\.m2\repository\org\hsqldb\hsqldb\2.2.4\hsqldb-2.2.4.jar" org.hsqldb.Server -database.0 data\testdb -dbname.0 testdb
--- src/test/java/de/gzockoll/spielwiese/entity/roles/DealerTest.java
+++ src/test/java/de/gzockoll/spielwiese/entity/roles/DealerTest.java
@@ -1,6 +1,5 @@
 package de.gzockoll.spielwiese.entity.roles;
 
-import static org.junit.Assert.fail;
 import junit.framework.Assert;
 
 import org.junit.After;
@@ -12,33 +11,33 @@ import de.gzockoll.spielwiese.entity.actor.Organization;
 
 public class DealerTest {
 
-	private Organization org;
+    private Organization org;
 
-	@Before
-	public void setUp() throws Exception {
-		org = new Organization("JUnit GmbH");
-		new Dealer(org);
-		Address a=new Address("Abcstr", "13", "12345", "Kleinkleckersdorg");
-		org.setAddress(a);
-	}
+    @Before
+    public void setUp() throws Exception {
+        org = new Organization("JUnit GmbH");
+        new Dealer(org);
+        Address a = new Address("Abcstr", "13", "12345", "Kleinkleckersdorf");
+        org.setAddress(a);
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@Test
-	public void testGetBillingAddress() {
-		Organization z=new Organization("Zentrale");
-		Dealer d = new Dealer(z);
-		Address a=new Address("Xyzstr", "1", "98765", "Groﬂkleckersdorg");
-		z.setAddress(a);
-		
-		Dealer d1=(Dealer) org.asRole("Dealer");
-		Assert.assertEquals("Abcstr", d1.getBillingAddress().getStrasse());
-		
-		d1.setZentrale(d);
-		Assert.assertEquals("Xyzstr", d1.getBillingAddress().getStrasse());
-			
-	}
+    @Test
+    public void testGetBillingAddress() {
+        Organization z = new Organization("Zentrale");
+        Dealer d = new Dealer(z);
+        Address a = new Address("Xyzstr", "1", "98765", "Gro√ükleckersdorf");
+        z.setAddress(a);
+
+        Dealer d1 = (Dealer) org.asRole("Dealer");
+        Assert.assertEquals("Abcstr", d1.getBillingAddress().getStrasse());
+
+        d1.setZentrale(d);
+        Assert.assertEquals("Xyzstr", d1.getBillingAddress().getStrasse());
+
+    }
 
 }
